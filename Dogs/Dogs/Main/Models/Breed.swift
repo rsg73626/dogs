f05e3111b27eea1057326ca069e8615feeda1089
Renan Soares Germano <renan.rsg@hotmail.com>
2022-01-03 23:01:11 -0300

    Creates the models `Breed` and `Image`.

---
+++ Dogs/Dogs/Main/Models/Breed.swift
@@ -0,0 +1,13 @@
+//
+//  Breed.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import Foundation
+
+struct Breed {
+    let name: String
+    let image: Image
+}
---
+++ Dogs/Dogs/Main/Models/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import Foundation
+
+enum Image {
+    case named(name: String)
+    case data(data: Data)
+}
