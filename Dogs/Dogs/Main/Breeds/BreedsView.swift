0db98ddaebfe68b7b7d41c7e42215388896a9f29
Renan Soares Germano <renan.rsg@hotmail.com>
2022-01-03 23:11:27 -0300

    Creates `BreedsView` and define it as the initial view controller programmatically.

---
+++ Dogs/Dogs/Main/Breeds/BreedsView.swift
@@ -0,0 +1,20 @@
+//
+//  BreedsView.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import UIKit
+
+final class BreedsView: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
---
+++ Dogs/Dogs/Main/Builders/InitialViewBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  InitialViewBuilder.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import UIKit
+
+struct InitialViewBuilder {
+
+    private init() { }
+
+    static func build() -> UIViewController {
+        BreedsView()
+    }
+}
