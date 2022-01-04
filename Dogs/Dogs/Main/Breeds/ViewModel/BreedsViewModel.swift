16efe8fcdbda1f8c552898ff6b4c5668ae14632f
Renan Soares Germano <renan.rsg@hotmail.com>
2022-01-03 23:15:25 -0300

    Creates the `BreedsViewModel` and the `Box` types.

---
+++ Dogs/Dogs/Main/Breeds/ViewModel/BreedsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BreedsViewModel.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import Foundation
+
+final class BreedsViewModel {
+
+}
---
+++ Dogs/Dogs/Main/Models/Box.swift
@@ -0,0 +1,30 @@
+//
+//  Box.swift
+//  Dogs
+//
+//  Created by Temporary Account on 03/01/22.
+//
+
+import Foundation
+
+class Box<T> {
+
+    typealias Observer = (T) -> Void
+
+    var observer: Observer?
+
+    var value: T {
+        didSet {
+            observer?(value)
+        }
+    }
+
+    init(value: T) {
+        self.value = value
+    }
+
+    func bind(_ observer: Observer?) {
+        self.observer = observer
+        observer?(value)
+    }
+}
