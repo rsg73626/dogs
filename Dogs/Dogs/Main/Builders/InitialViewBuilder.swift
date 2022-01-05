71366c13ca8ee6cf40fcb700e3a264226b1fcbf4
Renan Soares Germano <renan.rsg@hotmail.com>
2022-01-05 03:36:13 -0300

    Adjusting InitialViewBuilder.

--- Dogs/Dogs/Main/Builders/InitialViewBuilder.swift
+++ Dogs/Dogs/Main/Builders/InitialViewBuilder.swift
@@ -12,6 +12,10 @@ struct InitialViewBuilder {
     private init() { }
 
     static func build() -> UIViewController {
-        BreedsView()
+        let url = URL(string: "https://api.thedogapi.com/v1/breeds")!
+        let key = "971ba769-29e2-4a33-895e-9359c499444c"
+        let api = TheDogAPI(httpClient: URLSessionHTTPClient(), url: url, apiKey: key)
+        let service = BreedsServiceAdapter(api: api)
+        return BreedsViewBuilder.build(service: service)
     }
 }
